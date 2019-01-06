3b86cc43dc9695064878fca81f488eafc3ec67f2
Prince Moses Rajendran <princemosesrajendran@gmail.com>
2019-01-06 16:41:32 +0530

    Initial Commit

---
+++ src/main/java/com/demo/testcoverage/jacobo/ArithmeticOperations.java
@@ -0,0 +1,8 @@
+package com.demo.testcoverage.jacobo;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/com/demo/testcoverage/jacobo/ArithmeticOperationsTest.java
@@ -0,0 +1,19 @@
+package com.demo.testcoverage.jacobo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArithmeticOperationsTest {
+
+	@Test
+	public void testAdd() {
+
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.add(2, 6);
+		Integer expected = 8;
+		assertEquals(expected, actual);
+
+	}
+
+}
